42ed23c98f7a3c65b592d57db2c4afc58931cff6
YannickPolela <BIT21211632@stud.unilus.ac.zm>
2025-05-29 17:46:28 +0200

    Home screen edit

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,7 +8,22 @@ export default function Home() {
 return (
   <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
     <div className="relative w-[280px] h-[240px] lg:w-[454px] lg:h-[424px] mb-8 lg:mb-0">
-      <Image src ="/DevGuru_Mascot_2.png" fill alt = "Hero"/>
+      <video
+            className="rounded-md"
+            width="300"
+            height="300"
+            autoPlay
+            loop
+            muted
+            playsInline
+            disablePictureInPicture
+            controlsList="nodownload nofullscreen noremoteplayback"
+            onContextMenu={(e) => e.preventDefault()}
+            >
+            <source src="/Mascot_home.mp4" type="video/mp4" />
+            Mascot
+          </video>
+
 
     </div>
     <div className="flex flex-col items-center gap-y-8">
